ea589dc5e25893646261bc29b1f5745cedb28dfb
Andrej Plávka <andrej.p.j@gmail.com>
2021-11-22 23:04:38 +0100

    about text

--- src/tunesapp/views/About.tsx
+++ src/tunesapp/views/About.tsx
@@ -12,7 +12,7 @@ export const About = () => {
       <h1>{texth1}</h1>
 
       <p>
-        This is a practise <span>SinglePage</span> app project writen in{" "}
+        This is a practise <span>SinglePage</span> app project written in{" "}
         <span>React</span> ( create-react-app ) {"&"} <span>TypeScript</span>{" "}
         styled by <span>SCSS</span>. Structure is made by using {"=>"}{" "}
         functions, components {"&"} hooks in TDD flow.
