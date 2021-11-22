0b1a38c30de951a273417a3ed2f50779703fa332
Andrej Plávka <andrej.p.j@gmail.com>
2021-11-22 01:24:40 +0100

    about icons

--- src/a/components/about/HobbiesAb.tsx
+++ src/a/components/about/HobbiesAb.tsx
@@ -30,8 +30,6 @@ const DivMain = styled.div`
   flex-flow: row nowrap;
   justify-content: center;
   align-items: center;
-  width: auto;
-  height: 100%;
   padding: 0.6em;
   border-radius: 2px;
   background: ${theme.box_bg_w};
@@ -39,25 +37,19 @@ const DivMain = styled.div`
   filter: grayscale(100%);
   opacity: 0.65;
   div {
-    display: flex;
     width: 2em;
-    height: 100%;
+    height: 2em;
     margin: 0 5px 0 0;
   }
   span {
     font-size: 1.25em;
     margin: 0 0.1em;
   }
+
   :hover {
     filter: grayscale(0%);
     opacity: 1;
     transition: transform 0.15s linear;
     transform: translateY(3px);
   }
-  /* @media screen and (${theme.sx_min_425}) {
-  }
-  @media screen and (${theme.sm_min_768}) {
-  }
-  @media screen and (${theme.md_min_1024}) {
-  } */
 `;
